db258678d9651bf3d171709c3e45b4fc2c227f03
Bulatus <121303761+ryusandorosu@users.noreply.github.com>
2024-02-06 02:10:24 +0300

    Delete tree directory

--- tree/tests.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "team_tree.h"
-
-TEST(TreeIteratorTest, DereferenceOperatorConstEmptyTree) {
-  team::Tree<std::string> tree;
-  EXPECT_EQ(*tree.begin(), "");
-}
